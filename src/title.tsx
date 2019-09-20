ea6bbcd032e60f122ec573a24dcb5afca03d5e44
Joosung Park <admin@slowmotion.dev>
2019-09-20 10:51:37 +0900

    Remove: Useless constructor

--- src/title.tsx
+++ src/title.tsx
@@ -6,10 +6,6 @@ export interface ITitleProps {
 }
 
 export class Title extends Component<ITitleProps> {
-    constructor(props: ITitleProps) {
-        super(props);
-    }
-
     public render(): ReactNode {
         return <div className="Title">{this.props.value.toString()}{this.props.unit}의 쓰레기가 버려졌어요!</div>;
     }
